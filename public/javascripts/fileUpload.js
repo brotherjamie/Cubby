b6aa0b2ead3adb613013fa13d97e219dd165a09a
brotherjamie <jamie.vaughan79@gmail.com>
2022-03-02 12:55:15 +0000

    CSS tidy ups

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -4,10 +4,4 @@ FilePond.registerPlugin(
     FilePondPluginFileEncode,
   )
   
-  // FilePond.setOptions({
-  //   stylePanelAspectRatio: 150 / 100,
-  //   imageResizeTargetWidth: 100,
-  //   imageResizeTargetHeight: 150
-  // })
-  
   FilePond.parse(document.body);
